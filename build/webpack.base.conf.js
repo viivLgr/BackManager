821bf48a8bb378f9e8044cf806b911084649cc34
李果然 <viiv_lgr@163.com>
2018-06-13 13:35:01 +0800

    font-awesome

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -78,11 +78,11 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 80000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+        loader: 'file?name=fonts/[hash].[ext]',
+        // options: {
+        //   limit: 80000,
+        //   name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        // }
       }
     ]
   },

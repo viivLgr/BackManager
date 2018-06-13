726f1cdf61bcb741da83f994a1d82c5cd44a80c0
李果然 <viiv_lgr@163.com>
2018-06-13 12:10:20 +0800

    build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    // assetsSubDirectory: 'static',
+    assetsSubDirectory: '//s.payadmin.viivlgr.cn/BackManager/dist/',
     assetsPublicPath: './',
 
     /**

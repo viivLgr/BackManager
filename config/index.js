efe3f0c1d0aa16e68f3e6b9aec104519b51ca5c2
李果然 <viiv_lgr@163.com>
2018-07-24 18:15:59 +0800

    生产环境路由加前缀，打包不加前缀

--- config/index.js
+++ config/index.js
@@ -51,8 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: './',
-    assetsPublicPath: '/manage-front/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
